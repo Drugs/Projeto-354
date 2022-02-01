0958901d4c197af522fba9eca330228443d68fe5
Sh4tgun <98188746+Sh4tgun@users.noreply.github.com>
2022-02-01 15:54:58 -0300

    mudança

--- loja/tcchomepage.php
+++ loja/tcchomepage.php
@@ -83,7 +83,7 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-     <img src="" alt="" />
+     <img src="./img/pijama1.png" alt="Deu ruim" />
       <div class="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
