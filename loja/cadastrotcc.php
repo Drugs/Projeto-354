31112ed4b978d8c66693db4c19a8455daea0bf9b
GabrielBarbosa123 <Gabrielsb200705@gmail.com>
2022-02-04 16:23:57 -0300

    senha

--- loja/cadastrotcc.php
+++ loja/cadastrotcc.php
@@ -5,7 +5,7 @@ $title = 'Cadastro';
 if(isset($_POST) and $_POST != null){
 	var_dump($_POST);
 	$email = $_POST['email'];
-	$senha = $_POST['senha'];
+	$senha =base64_encode $_POST['senha'];
 	$nome = $_POST['nome'];
 	$cpf  = $_POST['cpf'];
 	$nascimento = $_POST['nascimento'];
@@ -31,13 +31,11 @@ if(isset($_POST) and $_POST != null){
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title><?=$title?></title>
+	<title>Cadastro</title>
 	<link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"  rel="stylesheet">
 </head>
-<body>
 <header>
-
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="#"><font color="#e92187">Tia Cleide Store</font></a>
@@ -60,66 +58,27 @@ if(isset($_POST) and $_POST != null){
     </div>
   </nav>
 </header>
-
-<div id="cadastro"><br><br><br>
-        <form method="post" action=""> 
-          <h1>Cadastro</h1> 
-           
-          <p> 
-            <label for="nome">Seu nome<br/></label>
-            <input id="nome" class="form-control mb-3" type="text" name="nome" required="required" placeholder="Careca Careca" />
-          </p>
-           
-          <p> 
-            <label for="email">Seu e-mail<br/></label>
-            <input id="email" class="form-control mb-3" type="email" name="email" required="required" placeholder="contato@gmail.com"/> 
-          </p>
-           
-          <p> 
-            <label for="senha">Sua senha<br/></label> 
-            <input id="senha" class="form-control mb-3" type="password" name="senha" required="required placeholder=Ex: 123"/>
-          </p>
-		  
-		    <p> 
-            <label for="cpf">Cpf<br/></label>
-            <input id="cpf" class="form-control mb-3" type="text" name="cpf" required="required" placeholder="000.000.008-00" />
-          </p>
-		  
-		  <p> 
-            <label for="nascimento">Data de Nascimento<br/></label>
-            <input id="nascimento" class="form-control mb-3" type="date" name="nascimento" required="required"/>
-          </p>
-		  
-		  <p> 
-            <label for="cep">Cep<br/></label>
-            <input id="cep" class="form-control mb-3" type="text" name="cep" required="required" placeholder="000.000.008-00" />
-          </p>
-		  
-		  <p> 
-            <label for="telefone">Número de Telefone<br/></label>
-            <input id="telefone" class="form-control mb-3" type="text" name="telefone" required="required" placeholder="11984560978 " />
-          </p>
-		  
-		  <p> 
-            <label for="endereço">Endereço<br/></label>
-            <input id="endereço" class="form-control mb-3" type="text" name="endereço" required="required" placeholder="Ex: Avenida Lins de Vasconcelos, 234; Avenida Paulista, 316, apartamento 13. " />
-          </p>
-		  
-			
-		  
-		  
-           
-          <p> 
-            <input type="submit" value="Cadastrar"/> 
-          </p>
-           
-          <p class="link">  
-            Já tem conta?
-            <a href="logintcc.php"> Ir para Login </a>
-          </p>
-        </form>
-      </div>
-    </div>
-  </div>  
+<body class="text-center"><br>
+	<div class="container">
+		<h2 class="mt-5">Cadastro</h2><br>
+		<div class="row justify-content-center">
+			<div class="col-sm-8 col-md-6 col-lg-4">
+				<form action="valida.php" method="post">
+					<input placeholder="Nome" class="form-control mb-3" type="text" name="nome" />
+					<input placeholder="Email" class="form-control mb-3" type="email" name="email" />
+					<input  placeholder="Senha" class="form-control mb-3" type="password" name="senha" />
+					<input placeholder="CPF" class="form-control mb-3" type="text" name="cpf" />
+					<input class="form-control mb-3" type="date" name="nascimento" />
+					<input placeholder="CEP" class="form-control mb-3" type="text" name="cep" />
+					<input placeholder="Telefone" class="form-control mb-3" type="text" name="telefone" />
+					<input placeholder="Endereço" class="form-control mb-3" type="text" name="endereço" />
+					
+					<a class="btn btn-lg btn-primary" href="#">Enviar</a>
+					<p class=" link">Não tem conta?<a href="cadastrotcc.php"> Cadastre-se </a>
+				</p>
+				</form>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
