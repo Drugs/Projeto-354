a03e48671e2331a3416f31b231febe9184ef10e9
GabrielBarbosa123 <Gabrielsb200705@gmail.com>
2022-02-08 16:21:08 -0300

    interno

--- usuario/interno.php
+++ usuario/interno.php
@@ -1,18 +1,8 @@
-<?php
-	include "../includes/conexao.php";
-	
-$sql = 'Select * from pessoa';
-$query = mysqli_query($mysql , $sql);
-$result = mysqli_fetch_all($query, MYSQLI_ASSOC);
-?>
-
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
 	<meta charset="UTF-8">
-	<title>Lista de pessoas</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-		<link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
+	<link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"  rel="stylesheet">
 	<link href="../css/carousel.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
@@ -22,38 +12,51 @@ $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
 	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
 	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
 	<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+	<title>Usuário</title>
+	<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/projeto-354/"><img src= "../img/jesonelputo.png"alt="Image" height="25" width="25"><font color="#e92187">Tia Cleide Store</font></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled"></a>
+          </li>
+        </ul>
+		<form class="d-flex">
+		<a class="btn btn-outline-success  me-2" href="/projeto-354/"><font color="#e92187" type="submit">Voltar</font></a>
+		</form>
+        <form class="d-flex">
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
 </head>
+
 <body>
-	<div class="container">
-		<h2  class="my-3">Seus Dados</h2>
-		<table class="my-4 table">
-			<tr class="table-dark">
-				<th>Pessoa</th>
-				<th>Email</th>
-				<th>CPF</th>
-				<th>Nascimento</th>
-				<th>Endereço</th>
-				<th>CEP</th>
-				<th>Telefone</th>
-				<th>Editar</th>
-				<th>Deletar</th>
-			</tr>
-		<?php
-			foreach($result as $linha) {
-				echo '<tr>';
-				echo '<td>' .$linha['nome']. '</td>';
-				echo '<td>' .$linha['email']. '</td>';
-				echo '<td>' .$linha['cpf']. '</td>';
-				echo '<td>' .$linha['nascimento']. '</td>';
-				echo '<td>' .$linha['endereço']. '</td>';
-				echo '<td>' .$linha['cep']. '</td>';
-				echo '<td>' .$linha['telefone']. '</td>';
-				echo '<td><a class="btn btn-warning"
-				href = "editar.php?id='.$linha['id_pessoa'].'">Editar</a></td>';
-				echo '<td><a class="btn btn-danger"
-				href="apagar.php?id='.$linha['id_pessoa'].'" >Apagar</a></td>';
-				echo '</tr>';
-			}
-		?>	
+
+<p><a class="btn btn-lg btn-primary mt-5" href="#">Editar Pessoa</a></p>
+<p><a class="btn btn-lg btn-primary mt-5" href="#">Editar Usuário</a></p>
+<p><a class="btn btn-lg btn-primary mt-5" href="#">Carrinho</a></p>
+<p><a class="btn btn-lg btn-primary mt-5" href="#">Logout</a></p>
+	<div class="container mt-4">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top mt-4">
+    <p class="col-md-4 mb-0 text-muted">© 2021 Company, Inc</p>
+
+    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+    </a>
+	<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><i class="bi bi-twitter"></i></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><i class="bi bi-instagram"></i></svg></a></li>
+      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><i class="bi bi-facebook"></i></svg></a></li>
+    </ul>
+  </footer>
+</div>
 </body>
 </html>
