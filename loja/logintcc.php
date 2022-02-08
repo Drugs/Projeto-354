8be4c2d3ebeaf11b55ab790dd9123e088e657e8b
GabrielBarbosa123 <Gabrielsb200705@gmail.com>
2022-02-08 15:52:27 -0300

    pato em tudo

--- loja/logintcc.php
+++ loja/logintcc.php
@@ -16,7 +16,7 @@
 	<header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/projeto-354/""><font color="#e92187">Tia Cleide Store</font></a>
+     <a class="navbar-brand" href="#"><img src= "../img/jesonelputo.png"alt="Image" height="25" width="25"><font color="#e92187">Tia Cleide Store</font></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
