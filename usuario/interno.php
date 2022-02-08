0f870ff3d8dce6ab0199792218616e9476f962f7
GabrielBarbosa123 <Gabrielsb200705@gmail.com>
2022-02-08 17:54:44 -0300

    login cadastro pato

--- usuario/interno.php
+++ usuario/interno.php
@@ -1,9 +1,7 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
+<html lang="en-US"><head>
 	<meta charset="UTF-8">
 	<link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"  rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="../css/carousel.css" rel="stylesheet">
 		<link href="../css/interno.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
@@ -14,16 +12,16 @@
 	<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
 	<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
 	<title>Usuário</title>
-	<header>
+	</head><body><header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><img src= "../img/jesonelputobranco.png"alt="Image" height="18" width="18"><font color="#e92187">Tia Cleide Store</font></a>
+      <a class="navbar-brand" href="#"><img src="../img/jesonelputobranco.png" alt="Image" height="18" width="18"><font color="#e92187">Tia Cleide Store</font></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          </li>
+          
           <li class="nav-item">
             <a class="nav-link disabled"></a>
           </li>
@@ -37,12 +35,12 @@
     </div>
   </nav>
 </header>
-</head>
 
-<body>
 
-<div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark " style="width: 280px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+
+
+<div class="d-flex flex-column p-3 text-white bg-dark" style="position: absolute;flex-shrink: 0;top: -5px;width: 272px;height: 100%;">
+    <a href="/" class="d-flex align-items-center mb-3 mt-5 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
       <span class="fs-4 mt-4">Dados</span>
     </a>
@@ -50,14 +48,14 @@
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
         <a href="#" class="nav-link active" aria-current="page">
-          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+          <svg class="bi me-2" width="16" height="16" href="editarpessoa.php"></svg>
           Editar Pessoa
         </a>
       </li>
       <li>
         <a href="#" class="nav-link text-white">
           <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
-          Editar Usário
+          Editar Usuário
         </a>
       </li>
       <li>
@@ -76,7 +74,7 @@
     <hr>
     <div class="dropdown">
         <strong>mdo</strong>
-      </a>
+      
       <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
         <li><a class="dropdown-item" href="#">New project...</a></li>
         <li><a class="dropdown-item" href="#">Settings</a></li>
@@ -86,19 +84,6 @@
       </ul>
     </div>
   </div>
-	<div class="container">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top mt-4">
-    <p class="col-md-4 mb-0 text-muted">© 2021 Company, Inc</p>
 
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-    </a>
-	<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><i class="bi bi-twitter"></i></svg></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><i class="bi bi-instagram"></i></svg></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><i class="bi bi-facebook"></i></svg></a></li>
-    </ul>
-  </footer>
-</div>
-</body>
-</html>
+
+</body></html>
--- usuario/usuarioalt.php
+++ usuario/usuarioalt.php
@@ -84,19 +84,6 @@
       </ul>
     </div>
   </div>
-	<div class="container" style="/* box-sizing: border-box; */padding: calc(100% -274px);float: right;width: calc(100% - 278px);">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top mt-4">
-    <p class="col-md-4 mb-0 text-muted">© 2021 Company, Inc</p>
 
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-    </a>
-	<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg><i class="bi bi-twitter"></i></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg><i class="bi bi-instagram"></i></a></li>
-      <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg><i class="bi bi-facebook"></i></a></li>
-    </ul>
-  </footer>
-</div>
 
 </body></html>
