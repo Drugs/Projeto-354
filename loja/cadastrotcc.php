adb9abc2fae34e2811fc8ab5f862a9c1b45011a8
GabrielBarbosa123 <Gabrielsb200705@gmail.com>
2022-02-08 15:54:38 -0300

    pato consertado

--- loja/cadastrotcc.php
+++ loja/cadastrotcc.php
@@ -45,7 +45,7 @@ if(isset($_POST) and $_POST != null){
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <div class="container-fluid">
- <a class="navbar-brand" href="#"><img src= "../img/jesonelputo.png"alt="Image" height="25" width="25"><font color="#e92187">Tia Cleide Store</font></a>
+      <a class="navbar-brand" href="/projeto-354/"><img src= "../img/jesonelputo.png"alt="Image" height="25" width="25"><font color="#e92187">Tia Cleide Store</font></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -66,6 +66,7 @@ if(isset($_POST) and $_POST != null){
   </nav>
 </header>
 <br><img src= "../img/jesonelputo.png" alt="Image" height="60" width="60" >
+
 <body class="text-center">
 	<div class="container">
 		<h2 class="mt-5">Cadastro</h2>
